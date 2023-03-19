58aea702ffe1601dcee052d13c1c3c862ad6e0a6
muratAVC <110787002+muratAVC@users.noreply.github.com>
2023-03-19 15:22:28 +0300

    19-03-2023 saat 1522

--- src/test/java/test/FirstGitUp.java
+++ src/test/java/test/FirstGitUp.java
@@ -3,5 +3,8 @@ package test;
 public class FirstGitUp {
     public static void main(String[] args) {
         System.out.println("selam git hub");
+        System.out.println("erdogan abi buraya bazen birşeyler yaz ve gönder " +
+                "diğer arkadaşları da ekleyelim nasıl olur" +
+                "herpimin beraber çalışalım");
     }
 }
